c35c4345cbfc89ffa250bd0f952467498723547f
luisconcha <luisconchacuray@gmail.com>
2017-02-27 23:16:44 -0300

    Criando crud de roles e não deixar deletar role padrão admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,77 +16,86 @@
     <!-- Scripts -->
     <script>
         window.Laravel = <?php echo json_encode( [
-					'csrfToken' => csrf_token(),
-				] ); ?>
+          'csrfToken' => csrf_token(),
+        ] ); ?>
     </script>
 </head>
 <body>
 <div id="app">
-		<?php
-		$navbar = Navbar::withBrand( config( 'app.name' ), url( '/home' ) )->inverse();
-		if ( Auth::check() ) {
-				$links  = Navigation::links( [
-					[
-						'User',
-						[
-							[
-								'link'  => route( 'laccuser.users.index' ),
-								'title' => 'User'
-							],
-							[
-								'link'  => route( 'laccuser.advanced.users.search' ),
-								'title' => 'Advanced Search'
-							]
-						]
-					],
-					[
-						'Category',
-						[
-							[
-								'link'  => route( 'categories.index' ),
-								'title' => 'List of Categories'
-							],
-							[
-								'link'  => route( 'trashed.categories.index' ),
-								'title' => 'Categories Trash'
-							]
-						]
-					],
-					[
-						'Book',
-						[
-							[
-								'link'  => route( 'books.index' ),
-								'title' => 'List of books'
-							],
-							[
-								'link'  => route( 'trashed.books.index' ),
-								'title' => 'Books Trash'
-							]
-						]
-					]
-				] );
-				$logout = Navigation::links( [
-					[
-						Auth::user()->name,
-						[
-							[
-								'link'           => url( '/logout' ),
-								'title'          => 'Logout',
-								'linkAttributes' => [
-									'onClick' => "event.preventDefault();document.getElementById(\"logout-form\").submit();"
-								]
-							],
-							[
-								'link'  => route( 'laccuser.users.edit', [ 'id' => Auth::user()->id ] ),
-								'title' => 'Edit profile'
-							]
-						],
-					]
-				] )->right();
-				$navbar->withContent( $links )->withContent( $logout );
-		}
-		?>
+    <?php
+    $navbar = Navbar::withBrand( config( 'app.name' ), url( '/home' ) )->inverse();
+    if ( Auth::check() ) {
+        $links  = Navigation::links( [
+          [
+            'User',
+            [
+              [
+                'link'  => route( 'laccuser.users.index' ),
+                'title' => 'User'
+              ],
+              [
+                'link'  => route( 'laccuser.advanced.users.search' ),
+                'title' => 'Advanced Search'
+              ]
+            ]
+          ],
+          [
+            'Category',
+            [
+              [
+                'link'  => route( 'categories.index' ),
+                'title' => 'List of Categories'
+              ],
+              [
+                'link'  => route( 'trashed.categories.index' ),
+                'title' => 'Categories Trash'
+              ]
+            ]
+          ],
+          [
+            'Book',
+            [
+              [
+                'link'  => route( 'books.index' ),
+                'title' => 'List of books'
+              ],
+              [
+                'link'  => route( 'trashed.books.index' ),
+                'title' => 'Books Trash'
+              ]
+            ]
+          ],
+          [
+            'Roles',
+            [
+              [
+                'link'  => route( 'laccuser.role.roles.index' ),
+                'title' => 'List of roles'
+              ]
+            ]
+          ]
+        ] );
+        $logout = Navigation::links( [
+          [
+            Auth::user()->name,
+            [
+              [
+                'link'           => url( '/logout' ),
+                'title'          => 'Logout',
+                'linkAttributes' => [
+                  'onClick' => "event.preventDefault();document.getElementById(\"logout-form\").submit();"
+                ]
+              ],
+              [
+                'link'  => route( 'laccuser.users.edit', [ 'id' => Auth::user()->id ] ),
+                'title' => 'Edit profile'
+              ]
+            ],
+          ]
+        ] )->right();
+        $navbar->withContent( $links )->withContent( $logout );
+    }
+    ?>
     {!! $navbar !!}
     {!! Form::open(['url' => url('/logout'), 'id' => 'logout-form', 'style' => 'display:none']) !!}
     {!! Form::close() !!}
